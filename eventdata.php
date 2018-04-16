d783d62a036ba6054ed5e8691e22232480533661
Aaratisalave <Aaratisalave@gmail.com>
2018-04-16 17:05:59 +0530

    eventdata

---
+++ eventdata.php
@@ -0,0 +1,190 @@
+<html>
+<style>
+body{
+   margin: 0;
+}
+
+header {
+
+  background: black;
+  color : white;
+  padding: 8px 0px 6px 40px;
+  height:45px;
+  
+}
+
+header h1 {
+
+  display: inline;
+  font-family:"Times New Roman", Times, serif;
+  font-size:36px;
+  font-weight:400;
+  margin-top:0px;
+  margin-right:10px;
+  float:left;
+  
+}
+
+nav ul{
+   
+   display:inline;
+   padding: 0px;
+   float:left;
+
+}
+
+nav ul li{
+   display: inline-block;
+   list-style-type:none;
+   color: #0000FF;
+   float:left;
+   margin-left:30px;
+   
+}
+
+
+nav ul li a{
+   color:#FFFFFF;
+   text-decoration:none;
+}
+
+.homered{
+  background-color: #FF0000;
+  padding: 30px 10px 18px 5px;
+}
+
+.divider {
+  background-color: #0066FF;
+  height: 5px; 
+}
+
+.homeblack:hover {
+  background-color: #0000FF;
+  padding:30px 10px 18px 10px;
+}
+
+#container{
+   width:60%;
+   height:450px;
+   border:1px solid black;
+   margin:0 auto;
+   position:relative;
+  
+}
+#container>img{
+    width:60%;
+	height:70%;
+	position:absolute;
+
+}
+.int1
+{
+	width:90%;
+	height:40%;
+	margin-left:40px;
+}
+.int
+{
+	
+	font-size:20px;
+	color:red;
+}
+.int1 h1
+{
+	margin:0 auto;
+}
+p1{
+				width:95%;
+				overflow:hidden;
+				margin:20px auto;
+				padding:20px 0px;
+				background-color:skyblue;
+			}
+			.p1 ul
+			{
+				padding:0px;
+				margin:0px;
+			}
+			.p1 ul li
+			{
+				list-style:none;
+				float:left;
+				width:20%;
+				height:300px;
+				margin:40px 20px 0px 40px;
+				box-sizing:border-box;
+				border:2px solid white
+				opacity:0.9;
+			}
+				
+.p1 ul li .title{
+		width:100%;
+		height:50px;
+		line-height:50px;
+		
+		text-align:center;
+		font-family:Italin;
+		font-size:30px;
+		font-style:bold
+}
+
+</style>
+<head>
+ <header>
+    <nav style="color:#FF0000;">
+    <h1>Student  and Alumni Management System</h1> 
+     <ul id="nav">
+	 <li><a class="homeblack" href="index.php">HOME</a></li>
+	 <li><a class="homeblack" href="eventdata.php">EVENTS</a></li>
+	 <li><a class="homeblack" href="about.php">ABOUT US</a></li>
+	 <li><a class="homeblack" href="adminlogin.php">ADMIN</a></li>
+	 <li><a class="homeblack" href="login.php">LOGIN</a></li>
+	 </ul>
+    </nav>
+</header>
+<head>
+<body style="background-image:url(image/i7.jpg)">
+
+<div class="int1">
+			<h1 style="color:#FFFFFF;" align="left">Alumni Meet</h1>
+			
+	<p id="int"><h3 style="color:#FFFFFF;" align="center">
+	<br> 
+	Place : ADCET, Ashta</br>
+	 </h3>
+	<h3 style="color:#FFFFFF;" align="center">Time: 10 pm</h3>
+	<h3 style="color:#FFFFFF;" align="center"> </br>
+      <br>
+	  List </br>
+	  <br>
+  </h3>
+	<table width="453" height="339" border="0" align="center">
+      <tr>
+        <td><div style="color:#FFFFFF;" align="center">1. Inaugration</div></td>
+      </tr>
+      <tr>
+        <td><div style="color:#FFFFFF;" align="center">2. Felicitation</div></td>
+      </tr>
+      <tr>
+        <td><div style="color:#FFFFFF;" align="center">3. Prize Distribution </div></td>
+      </tr>
+      <tr>
+        <td><div style="color:#FFFFFF;" align="center">4.Speech</div></td>
+      </tr>
+      <tr>
+        <td><div style="color:#FFFFFF;" align="center">5.Lunch</div></td>
+      </tr>
+      <tr>
+        <td><div style="color:#FFFFFF;" align="center">6.Photo Session </div></td>
+      </tr>
+    </table>
+  <h3 align="left"><br>
+  </h3>
+	</p>
+</div>
+
+			</ul>
+		</div>
+</body>
+<html>
+
